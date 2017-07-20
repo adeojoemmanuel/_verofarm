197552b135d22d9a32a9073b788263ef3d5f8fc8
DESKTOP-TL741B2\IM <emmanuel.adeojo@yahoo.com>
2017-07-21 00:11:13 +0100

    changed script loc

--- index.php
+++ index.php
@@ -16,6 +16,51 @@
 		<!-- Custom Css -->
 		<link rel="stylesheet" type="text/css" href="css/style.css">
 		<link rel="stylesheet" type="text/css" href="css/responsive.css">
+		<!-- Js File_________________________________ -->
+
+		<!-- j Query -->
+		<script type="text/javascript" src="js/jquery-2.1.4.js"></script>
+		<!-- Bootstrap JS -->
+		<script type="text/javascript" src="js/bootstrap.min.js"></script>
+
+		<!-- Vendor js _________ -->
+		<!-- Google map js -->
+		<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCRvBPo3-t31YFk588DpMYS6EqKf-oGBSI"></script> <!-- Gmap Helper -->
+		<script src="js/gmap.js"></script>
+		<!-- owl.carousel -->
+		<script type="text/javascript" src="js/owl.carousel.min.js"></script>
+		<!-- ui js -->
+		<script type="text/javascript" src="js/jquery-ui.min.js"></script>
+		<!-- Responsive menu-->
+		<script type="text/javascript" src="js/menuzord.js"></script>
+		<!-- revolution -->
+		<script src="vendor/revolution/jquery.themepunch.tools.min.js"></script>
+		<script src="vendor/revolution/jquery.themepunch.revolution.min.js"></script>
+		<script type="text/javascript" src="vendor/revolution/revolution.extension.slideanims.min.js"></script>
+		<script type="text/javascript" src="vendor/revolution/revolution.extension.layeranimation.min.js"></script>
+		<script type="text/javascript" src="vendor/revolution/revolution.extension.navigation.min.js"></script>
+		<script type="text/javascript" src="vendor/revolution/revolution.extension.kenburn.min.js"></script>
+		<script type="text/javascript" src="vendor/revolution/revolution.extension.actions.min.js"></script>
+		<script type="text/javascript" src="vendor/revolution/revolution.extension.parallax.min.js"></script>
+		<script type="text/javascript" src="vendor/revolution/revolution.extension.migration.min.js"></script>
+
+		<!-- landguage switcher js -->
+		<script type="text/javascript" src="js/jquery.polyglot.language.switcher.js"></script>
+		<!-- Fancybox js -->
+		<script type="text/javascript" src="js/jquery.fancybox.pack.js"></script>
+		<!-- js count to -->
+		<script type="text/javascript" src="js/jquery.appear.js"></script>
+		<script type="text/javascript" src="js/jquery.countTo.js"></script>
+		<!-- WOW js -->
+		<script type="text/javascript" src="js/wow.min.js"></script>
+
+		<script type="text/javascript" src="js/SmoothScroll.js"></script>
+
+		<script src="js/bootstrap-select.min.js"></script>
+		<script src="js/jquery.mixitup.min.js"></script>
+		<!-- Theme js -->
+		<script type="text/javascript" src="js/theme.js"></script>
+		<script type="text/javascript" src="js/google-map.js"></script>
 
 
 		<!-- Fixing Internet Explorer ______________________________________-->
@@ -1183,51 +1228,7 @@
 
 
 
-		<!-- Js File_________________________________ -->
-
-		<!-- j Query -->
-		<script type="text/javascript" src="js/jquery-2.1.4.js"></script>
-		<!-- Bootstrap JS -->
-		<script type="text/javascript" src="js/bootstrap.min.js"></script>
-
-		<!-- Vendor js _________ -->
-		<!-- Google map js -->
-		<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCRvBPo3-t31YFk588DpMYS6EqKf-oGBSI"></script> <!-- Gmap Helper -->
-		<script src="js/gmap.js"></script>
-		<!-- owl.carousel -->
-		<script type="text/javascript" src="js/owl.carousel.min.js"></script>
-		<!-- ui js -->
-		<script type="text/javascript" src="js/jquery-ui.min.js"></script>
-		<!-- Responsive menu-->
-		<script type="text/javascript" src="js/menuzord.js"></script>
-		<!-- revolution -->
-		<script src="vendor/revolution/jquery.themepunch.tools.min.js"></script>
-		<script src="vendor/revolution/jquery.themepunch.revolution.min.js"></script>
-		<script type="text/javascript" src="vendor/revolution/revolution.extension.slideanims.min.js"></script>
-		<script type="text/javascript" src="vendor/revolution/revolution.extension.layeranimation.min.js"></script>
-		<script type="text/javascript" src="vendor/revolution/revolution.extension.navigation.min.js"></script>
-		<script type="text/javascript" src="vendor/revolution/revolution.extension.kenburn.min.js"></script>
-		<script type="text/javascript" src="vendor/revolution/revolution.extension.actions.min.js"></script>
-		<script type="text/javascript" src="vendor/revolution/revolution.extension.parallax.min.js"></script>
-		<script type="text/javascript" src="vendor/revolution/revolution.extension.migration.min.js"></script>
-
-		<!-- landguage switcher js -->
-		<script type="text/javascript" src="js/jquery.polyglot.language.switcher.js"></script>
-		<!-- Fancybox js -->
-		<script type="text/javascript" src="js/jquery.fancybox.pack.js"></script>
-		<!-- js count to -->
-		<script type="text/javascript" src="js/jquery.appear.js"></script>
-		<script type="text/javascript" src="js/jquery.countTo.js"></script>
-		<!-- WOW js -->
-		<script type="text/javascript" src="js/wow.min.js"></script>
-
-		<script type="text/javascript" src="js/SmoothScroll.js"></script>
-
-		<script src="js/bootstrap-select.min.js"></script>
-		<script src="js/jquery.mixitup.min.js"></script>
-		<!-- Theme js -->
-		<script type="text/javascript" src="js/theme.js"></script>
-		<script type="text/javascript" src="js/google-map.js"></script>
+		
 
 		</div> <!-- End of .main_page -->
 	</body>
